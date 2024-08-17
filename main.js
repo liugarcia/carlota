d44265a5fb0039e4ef7467232c9a84dfaf8e8f7b
liugarcia <118115099+liugarcia@users.noreply.github.com>
2024-08-16 22:30:05 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-import { arbitrum, mainnet } from '@wagmi/core/chains'
-import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi'
+import { createWeb3Modal, defaultConfig } from '@web3modal/ethers5'
 
 // @ts-expect-error 1. Get projectId
 const projectId = import.meta.env.VITE_PROJECT_ID
@@ -7,21 +6,45 @@ if (!projectId) {
   throw new Error('VITE_PROJECT_ID is not set')
 }
 
+function getBlockchainApiRpcUrl(chainId) {
+  return `https://rpc.walletconnect.org/v1/?chainId=eip155:${chainId}&projectId=${projectId}`
+}
+
 // 2. Create wagmiConfig
-const chains = [mainnet, arbitrum]
-const wagmiConfig = defaultWagmiConfig({
-  chains,
-  projectId,
-  metadata: {
-    name: 'Html Example',
-    description: 'Html Example',
-    url: 'https://web3modal.com',
-    icons: ['https://avatars.githubusercontent.com/u/37784886']
+const chains = [
+  {
+    chainId: 1,
+    name: 'Ethereum',
+    currency: 'ETH',
+    explorerUrl: 'https://etherscan.io',
+    rpcUrl: getBlockchainApiRpcUrl(1)
+  },
+  {
+    chainId: 42161,
+    name: 'Arbitrum',
+    currency: 'ETH',
+    explorerUrl: 'https://arbiscan.io',
+    rpcUrl: getBlockchainApiRpcUrl(42161)
   }
+]
+
+const ethersConfig = defaultConfig({
+  metadata: {
+    name: 'AppKit',
+    description: 'AppKit Laboratory',
+    url: 'https://example.com',
+    icons: ['https://avatars.githubusercontent.com/u/37784886']
+  },
+  defaultChainId: 1
 })
 
 // 3. Create modal
-const modal = createWeb3Modal({ wagmiConfig, projectId, chains, themeMode: 'light' })
+const modal = createWeb3Modal({
+  ethersConfig: { ...ethersConfig, email: true },
+  projectId,
+  chains,
+  themeMode: 'light'
+})
 
 // 4. Trigger modal programaticaly
 const openConnectModalBtn = document.getElementById('open-connect-modal')
