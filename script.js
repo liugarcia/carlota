35ac2e285ddd5cf39575e40247d4dea65cbe117a
liugarcia <118115099+liugarcia@users.noreply.github.com>
2024-08-16 21:49:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,35 @@
-import { createWeb3Modal, defaultConfig } from '@web3modal/ethers'
 
-// 1. Get projectId from https://cloud.walletconnect.com
-const projectId = 'ea82e406480d9d8f524c7fe5c20cd367'
+// main.ts
 
-// 2. Set chains
-const mainnet = {
-  chainId: 1,
-  name: 'Ethereum',
-  currency: 'ETH',
-  explorerUrl: 'https://etherscan.io',
-  rpcUrl: 'https://cloudflare-eth.com'
-}
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi'
 
-// 3. Create your application's metadata object
+import { mainnet, arbitrum } from 'viem/chains'
+import { reconnect } from '@wagmi/core'
+
+// Your WalletConnect Cloud project ID
+export const projectId = 'ea82e406480d9d8f524c7fe5c20cd367'
+
+// Create a metadata object
 const metadata = {
-  name: 'My Website',
-  description: 'My Website description',
-  url: 'https://carlota-six.vercel.app/', // url must match your domain & subdomain
-  icons: ['https://avatars.mywebsite.com/']
+  name: 'carlota',
+  description: 'AppKit Example',
+  url: 'https://web3modal.com', // origin must match your domain & subdomain
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
 }
 
-// 4. Create Ethers config
-const ethersConfig = defaultConfig({
-  /*Required*/
-  metadata,
-
-  /*Optional*/
-  enableEIP6963: true, // true by default
-  enableInjected: true, // true by default
-  enableCoinbase: true, // true by default
-  rpcUrl: '...', // used for the Coinbase SDK
-  defaultChainId: 1 // used for the Coinbase SDK
-})
-
-// 5. Create a AppKit instance
-const modal = createWeb3Modal({
-  ethersConfig,
-  chains: [mainnet],
+// Create wagmiConfig
+const chains = [mainnet, arbitrum] as const
+export const config = defaultWagmiConfig({
+  chains,
   projectId,
-  enableAnalytics: true // Optional - defaults to your Cloud configuration
+  metadata,
+  ...wagmiOptions // Optional - Override createConfig parameters
+})
+reconnect(config)
+
+const modal = createWeb3Modal({
+  wagmiConfig: config,
+  projectId,
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+  enableOnramp: true // Optional - false as default
 })
