0a30f3522f0f4d1a101a3a7b6a6a2645d4b424c3
liugarcia <118115099+liugarcia@users.noreply.github.com>
2024-08-16 22:21:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,115 @@
-import {
-  EthereumClient,
-  w3mConnectors,
-  w3mProvider,
-  WagmiCore,
-  WagmiCoreChains,
-  WagmiCoreConnectors,
-} from "https://unpkg.com/@web3modal/ethereum@2.7.1";
+import '@web3modal/polyfills'
+import type { Chain, Metadata, Provider, ProviderType } from '@web3modal/scaffold-utils/ethers'
+import { CoinbaseWalletSDK, type ProviderInterface } from '@coinbase/wallet-sdk'
+import type { SocialProvider } from '@web3modal/scaffold-utils'
 
-import { Web3Modal } from "https://unpkg.com/@web3modal/html@2.7.1";
+export interface ConfigOptions {
+  enableEIP6963?: boolean
+  enableCoinbase?: boolean
+  auth?: {
+    email?: boolean
+    socials?: SocialProvider[]
+    showWallets?: boolean
+    walletFeatures?: boolean
+  }
+  enableInjected?: boolean
+  /**
+   * @deprecated this doesn't do anything, use `chains` instead
+   */
+  rpcUrl?: string
+  defaultChainId?: number
+  metadata: Metadata
+  chains?: Chain[]
+  coinbasePreference?: 'all' | 'smartWalletOnly' | 'eoaOnly'
+}
 
-// 0. Import wagmi dependencies
-const { mainnet, polygon, avalanche, arbitrum } = WagmiCoreChains;
-const { configureChains, createConfig } = WagmiCore;
-
-// 1. Define chains
-const chains = [mainnet, polygon, avalanche, arbitrum];
-const projectId = "2aca272d18deb10ff748260da5f78bfd";
-
-// 2. Configure wagmi client
-const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors: [
-    ...w3mConnectors({ chains, version: 2, projectId }),
-    new WagmiCoreConnectors.CoinbaseWalletConnector({
-      chains,
-      options: {
-        appName: "html wagmi example",
-      },
-    }),
-  ],
-  publicClient,
-});
-
-// 3. Create ethereum and modal clients
-const ethereumClient = new EthereumClient(wagmiConfig, chains);
-export const web3Modal = new Web3Modal(
-  {
-    projectId,
-    walletImages: {
-      safe: "https://pbs.twimg.com/profile_images/1566773491764023297/IvmCdGnM_400x400.jpg",
+export function defaultConfig(options: ConfigOptions) {
+  const {
+    enableEIP6963 = true,
+    enableCoinbase = true,
+    enableInjected = true,
+    auth = {
+      email: true,
+      showWallets: true,
+      walletFeatures: true
     },
-  },
-  ethereumClient
-);
+    metadata
+  } = options
+
+  let injectedProvider: Provider | undefined = undefined
+
+  // eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
+  let coinbaseProvider: ProviderInterface | undefined = undefined
+
+  const providers: ProviderType = { metadata }
+
+  function getInjectedProvider() {
+    if (injectedProvider) {
+      return injectedProvider
+    }
+
+    if (typeof window === 'undefined') {
+      return undefined
+    }
+
+    if (!window.ethereum) {
+      return undefined
+    }
+
+    //  @ts-expect-error window.ethereum satisfies Provider
+    injectedProvider = window.ethereum
+
+    return injectedProvider
+  }
+
+  function getCoinbaseProvider() {
+    if (coinbaseProvider) {
+      return coinbaseProvider
+    }
+
+    if (typeof window === 'undefined') {
+      return undefined
+    }
+
+    const coinbaseWallet = new CoinbaseWalletSDK({
+      appName: metadata.name,
+      appLogoUrl: metadata.icons[0],
+      appChainIds: options.chains?.map(chain => chain.chainId) || [1, 84532]
+    })
+
+    /**
+     * Determines which wallet options to display in Coinbase Wallet SDK.
+     * @property options
+     *   - `all`: Show both smart wallet and EOA options.
+     *   - `smartWalletOnly`: Show only smart wallet options.
+     *   - `eoaOnly`: Show only EOA options.
+     * @see https://www.smartwallet.dev/sdk/v3-to-v4-changes#parameters
+     */
+    coinbaseProvider = coinbaseWallet.makeWeb3Provider({
+      options: options.coinbasePreference || 'all'
+    })
+
+    return coinbaseProvider
+  }
+
+  if (enableInjected) {
+    providers.injected = getInjectedProvider()
+  }
+
+  if (enableCoinbase) {
+    providers.coinbase = getCoinbaseProvider()
+  }
+
+  if (enableEIP6963) {
+    providers.EIP6963 = true
+  }
+
+  if (auth) {
+    auth.email ??= true
+    auth.showWallets ??= true
+    auth.walletFeatures ??= true
+    providers.auth = auth
+  }
+
+  return providers
+}
