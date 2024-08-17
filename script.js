ce4eea76d38310ff8bba53f4d5460fa17ee87739
liugarcia <118115099+liugarcia@users.noreply.github.com>
2024-08-16 22:23:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,115 +1,33 @@
-import '@web3modal/polyfills'
-import type { Chain, Metadata, Provider, ProviderType } from '@web3modal/scaffold-utils/ethers'
-import { CoinbaseWalletSDK, type ProviderInterface } from '@coinbase/wallet-sdk'
-import type { SocialProvider } from '@web3modal/scaffold-utils'
+import { arbitrum, mainnet } from '@wagmi/core/chains'
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi'
 
-export interface ConfigOptions {
-  enableEIP6963?: boolean
-  enableCoinbase?: boolean
-  auth?: {
-    email?: boolean
-    socials?: SocialProvider[]
-    showWallets?: boolean
-    walletFeatures?: boolean
-  }
-  enableInjected?: boolean
-  /**
-   * @deprecated this doesn't do anything, use `chains` instead
-   */
-  rpcUrl?: string
-  defaultChainId?: number
-  metadata: Metadata
-  chains?: Chain[]
-  coinbasePreference?: 'all' | 'smartWalletOnly' | 'eoaOnly'
+// @ts-expect-error 1. Get projectId
+const projectId = import.meta.env.VITE_PROJECT_ID
+if (!projectId) {
+  throw new Error('VITE_PROJECT_ID is not set')
 }
 
-export function defaultConfig(options: ConfigOptions) {
-  const {
-    enableEIP6963 = true,
-    enableCoinbase = true,
-    enableInjected = true,
-    auth = {
-      email: true,
-      showWallets: true,
-      walletFeatures: true
-    },
-    metadata
-  } = options
-
-  let injectedProvider: Provider | undefined = undefined
-
-  // eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
-  let coinbaseProvider: ProviderInterface | undefined = undefined
-
-  const providers: ProviderType = { metadata }
-
-  function getInjectedProvider() {
-    if (injectedProvider) {
-      return injectedProvider
-    }
-
-    if (typeof window === 'undefined') {
-      return undefined
-    }
-
-    if (!window.ethereum) {
-      return undefined
-    }
-
-    //  @ts-expect-error window.ethereum satisfies Provider
-    injectedProvider = window.ethereum
-
-    return injectedProvider
+// 2. Create wagmiConfig
+const chains = [mainnet, arbitrum]
+const wagmiConfig = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata: {
+    name: 'Html Example',
+    description: 'Html Example',
+    url: 'https://web3modal.com',
+    icons: ['https://avatars.githubusercontent.com/u/37784886']
   }
+})
 
-  function getCoinbaseProvider() {
-    if (coinbaseProvider) {
-      return coinbaseProvider
-    }
+// 3. Create modal
+const modal = createWeb3Modal({ wagmiConfig, projectId, chains, themeMode: 'light' })
 
-    if (typeof window === 'undefined') {
-      return undefined
-    }
+// 4. Trigger modal programaticaly
+const openConnectModalBtn = document.getElementById('open-connect-modal')
+const openNetworkModalBtn = document.getElementById('open-network-modal')
 
-    const coinbaseWallet = new CoinbaseWalletSDK({
-      appName: metadata.name,
-      appLogoUrl: metadata.icons[0],
-      appChainIds: options.chains?.map(chain => chain.chainId) || [1, 84532]
-    })
+openConnectModalBtn.addEventListener('click', () => modal.open())
+openNetworkModalBtn.addEventListener('click', () => modal.open({ view: 'Networks' }))
 
-    /**
-     * Determines which wallet options to display in Coinbase Wallet SDK.
-     * @property options
-     *   - `all`: Show both smart wallet and EOA options.
-     *   - `smartWalletOnly`: Show only smart wallet options.
-     *   - `eoaOnly`: Show only EOA options.
-     * @see https://www.smartwallet.dev/sdk/v3-to-v4-changes#parameters
-     */
-    coinbaseProvider = coinbaseWallet.makeWeb3Provider({
-      options: options.coinbasePreference || 'all'
-    })
-
-    return coinbaseProvider
-  }
-
-  if (enableInjected) {
-    providers.injected = getInjectedProvider()
-  }
-
-  if (enableCoinbase) {
-    providers.coinbase = getCoinbaseProvider()
-  }
-
-  if (enableEIP6963) {
-    providers.EIP6963 = true
-  }
-
-  if (auth) {
-    auth.email ??= true
-    auth.showWallets ??= true
-    auth.walletFeatures ??= true
-    providers.auth = auth
-  }
-
-  return providers
-}
+// 5. Alternatively use w3m component buttons (see index.html)
